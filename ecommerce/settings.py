faf98f5a32e8de3bcc466753798d704830541f34
Olatuyole <omoyeleolatuyole39@gmail.com>
2025-12-06 17:03:54 +0000

    added example.env

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -57,7 +57,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
-    'shop',
+    'shop', # Our custom app
 ]
 
 MIDDLEWARE = [
@@ -75,7 +75,7 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 EMAIL_HOST  = os.getenv("EMAIL_HOST", "smtp.gmail.com")
 EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
-EMAIL_USE_TLS = True        
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True") == "True"  
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
